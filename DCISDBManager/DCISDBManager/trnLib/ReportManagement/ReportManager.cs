4dc51eee451da230f1e7bd430bc13a28e9006d80
Tharaka <Tharaka@votreit.com>
2018-01-20 01:05:38 +0530

    Transatin Compare  Method added

--- DCISDBManager/DCISDBManager/trnLib/ReportManagement/ReportManager.cs
+++ DCISDBManager/DCISDBManager/trnLib/ReportManagement/ReportManager.cs
@@ -80,6 +80,73 @@ namespace DCISDBManager.trnLib.ReportManagement
 
         }
 
+        public DataTable getCORegistry_MonthlySummary(DateTime FDate, DateTime TDate, string CertRateID, string InvoSupDID, string InvoRID, string OthrDRID, string CID, string NCEM, string PayType)
+        {
+            try
+            {
+                connection.Open();
+
+                DataTable dt = new DataTable();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Connection = connection;
+
+                cmd.CommandText = "DCISgetCORegistryMnthSummry";
+
+                SqlParameter paraFrmDate = new SqlParameter("@FromDate", SqlDbType.VarChar);
+                SqlParameter paraToDate = new SqlParameter("@ToDate", SqlDbType.VarChar);
+                SqlParameter paraCertificateRateID = new SqlParameter("@CertificateRateId", SqlDbType.VarChar);
+                SqlParameter paraInvoicSDID = new SqlParameter("@InvoiceSupportingDocId", SqlDbType.VarChar);
+                SqlParameter paraInvoRateID = new SqlParameter("@InvoicerateId", SqlDbType.VarChar);
+                SqlParameter paraOtheDRID = new SqlParameter("@OtherDocumentRateId", SqlDbType.VarChar);
+                SqlParameter paraCustomerID = new SqlParameter("@CustomerId", SqlDbType.VarChar);
+                SqlParameter paraNCEMember = new SqlParameter("@NCEMember", SqlDbType.VarChar);
+                SqlParameter paraPayType = new SqlParameter("@Paytype", SqlDbType.VarChar);
+
+                paraFrmDate.Value = FDate.ToString("yyyyMMdd");
+                paraToDate.Value = TDate.ToString("yyyyMMdd");
+                paraCertificateRateID.Value = CertRateID;
+                paraInvoicSDID.Value = InvoSupDID;
+                paraInvoRateID.Value = InvoRID;
+                paraOtheDRID.Value = OthrDRID;
+                paraCustomerID.Value = CID;
+                paraNCEMember.Value = NCEM;
+                paraPayType.Value = PayType;
+
+                cmd.Parameters.Add(paraFrmDate);
+                cmd.Parameters.Add(paraToDate);
+                cmd.Parameters.Add(paraCertificateRateID);
+                cmd.Parameters.Add(paraInvoicSDID);
+                cmd.Parameters.Add(paraInvoRateID);
+                cmd.Parameters.Add(paraOtheDRID);
+                cmd.Parameters.Add(paraCustomerID);
+                cmd.Parameters.Add(paraNCEMember);
+                cmd.Parameters.Add(paraPayType);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                return dt;
+
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.LogError(ex);
+                return null;
+            }
+            finally
+            {
+
+                connection.Close();
+
+            }
+
+        }
+
         public DataTable getCustomerReport()
         {
             try
